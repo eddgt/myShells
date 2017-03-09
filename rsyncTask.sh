49bf6baaa23371881f5a7d49f0a3155eb0157156
Edmund <edmundo_ulloa@yahoo.com>
2017-03-09 11:44:34 -0600

    Agregando sincronizacion MSSQL
    Agregando sinc para respaldo MSSQL Scripts

--- rsyncTask.sh
+++
@@ -1,4 +0,0 @@
-
-#Sincroniza ficheros de /origen/ a /destino/
-
-rsync -rtvu /cygdrive/c/Users/oulloa/Documents/PLSQL_Scripts/ /cygdrive/c/Users/oulloa/Documents/MEGAsync/PLSQL_Scripts/
---
+++ sincMega.sh
@@ -0,0 +1,9 @@
+
+#Sincroniza ficheros de /origen/ a /destino/
+
+
+#Sincronizar PLSQL
+rsync -rtvu /cygdrive/c/Users/oulloa/Documents/PLSQL_Scripts/ /cygdrive/c/Users/oulloa/Documents/MEGAsync/PLSQL_Scripts/
+
+#Sincronizar MSSQL
+rsync -rtvu  '/cygdrive/c/Users/oulloa/Documents/SQL Server Management Studio/Projects/' '/cygdrive/c/Users/oulloa/Documents/MEGAsync/SQL Server Management Studio/Projects/'
