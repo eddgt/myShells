b2b59412a3f89a705041012225272e8e121758a2
Edmund <edmundo_ulloa@yahoo.com>
2017-03-09 11:30:22 -0600

    Modificando comentarios
    modificando comentarios de scripts y README.d

--- rsyncTask.sh
+++ rsyncTask.sh
@@ -1,2 +1,4 @@
-#Sincroniza fichoros de /origen/ a /destino/
+
+#Sincroniza ficheros de /origen/ a /destino/
+
 rsync -rtvu /cygdrive/c/Users/oulloa/Documents/PLSQL_Scripts/ /cygdrive/c/Users/oulloa/Documents/MEGAsync/PLSQL_Scripts/
