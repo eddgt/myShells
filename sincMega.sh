f7734b71c50eb0118b230345972e691196b6ae5a
U-COMCEL_DOMINIO\oulloa <oulloa@OULLOA-TF-DT.Comcel.com.gt>
2017-03-13 08:24:20 -0600

    actualizando scripts

--- sincMega.sh
+++ sincMega.sh
@@ -3,7 +3,9 @@
 
 
 #Sincronizar PLSQL
+echo "Sincronizando PL's..."
 rsync -rtvu /cygdrive/c/Users/oulloa/Documents/PLSQL_Scripts/ /cygdrive/c/Users/oulloa/Documents/MEGAsync/PLSQL_Scripts/
 
 #Sincronizar MSSQL
+echo "Sincronizando MSSQL's..."
 rsync -rtvu  '/cygdrive/c/Users/oulloa/Documents/SQL Server Management Studio/Projects/' '/cygdrive/c/Users/oulloa/Documents/MEGAsync/SQL Server Management Studio/Projects/'
